2f0d35635a83c6ebc534fb05c2d4f274cefa6ca4
Juan Escobar <escogil18@gmail.com>
2023-11-05 12:09:53 -0500

    Database

--- ProyectoIndicadores/controlador/configBd.php
+++ ProyectoIndicadores/controlador/configBd.php
@@ -2,7 +2,7 @@
 $GLOBALS['serv']="localhost";
 $GLOBALS['usua']="root";
 $GLOBALS['pass']="";
-$GLOBALS['bdat']="bd_indicadores";
+$GLOBALS['bdat']="bd_indicadores_1330";
 $GLOBALS['port']=3306;
 //$GLOBALS['serv']="serv1mysql.mysql.database.azure.com";
 //$GLOBALS['usua']="adminmysql@serv1mysql";
